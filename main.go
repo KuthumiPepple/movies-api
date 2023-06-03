2fed5426b02fa76afe9f91a1b8be29de916d030c
Kuthumi Pepple <kuthumipepple@gmail.com>
2023-06-03 12:31:49 +0100

    Add GET method to retrieve a movie by ID

--- main.go
+++ main.go
@@ -27,6 +27,16 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(movies)
 }
+func getMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies{
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
@@ -37,6 +47,7 @@ func deleteMovie(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 }
+
 func main() {
 	movies = append(movies, Movie{ID: "1", Isbn: "487391", Title: "First Movie", Director: &Director{Firstname: "Matt", Latname: "Mark"}})
 	movies = append(movies, Movie{ID: "2", Isbn: "463884", Title: "Second Movie", Director: &Director{Firstname: "Luke", Latname: "John"}})
@@ -45,6 +56,7 @@ func main() {
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 
 	fmt.Printf("Starting server at port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
